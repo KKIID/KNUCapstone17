4600022635c3dba0738ea84bdf62aa4036310839
Bist-PC\Bist <davilblood@naver.com>
2017-05-09 18:19:18 +0900

    Add floating message action

--- Sourcecode/Android/Logger_dy/app/src/main/java/kr/ac/bist/iot_noti/Activity/ScrollingActivity.java
+++ Sourcecode/Android/Logger_dy/app/src/main/java/kr/ac/bist/iot_noti/Activity/ScrollingActivity.java
@@ -1,34 +1,42 @@
 package kr.ac.bist.iot_noti.Activity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 import kr.ac.bist.iot_noti.Adapter.NotificationData;
 import kr.ac.bist.iot_noti.Adapter.NotificationDataAdaptor;
 import kr.ac.bist.iot_noti.R;
-import kr.ac.bist.iot_noti.Service.MyFirebaseMessagingService;
 import kr.ac.bist.iot_noti.messaging.ConnManager;
 
 public class ScrollingActivity extends AppCompatActivity {
@@ -36,9 +44,11 @@ public class ScrollingActivity extends AppCompatActivity {
     public static RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<NotificationData> myDataset;
-    private SwipeRefreshLayout reLayout;
+    //private SwipeRefreshLayout reLayout;
     public static Context mContext;
     private BroadcastReceiver myReceiver;
+    private int result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,15 +63,21 @@ public class ScrollingActivity extends AppCompatActivity {
         myDataset = new ArrayList<>();
         mAdapter = new NotificationDataAdaptor(myDataset);
         mRecyclerView.setAdapter(mAdapter);
+
+        permissionRequester();
+
+
         IntentFilter intentfilter = new IntentFilter();
         intentfilter.addAction("kr.ac.bist.iot_noti.fcmNotification");
         refreshData();
+
+
         myReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                    if(intent.getAction().equals("kr.ac.bist.iot_noti.fcmNotification")){
-                        refreshData();
-                    }
+                if (intent.getAction().equals("kr.ac.bist.iot_noti.fcmNotification")) {
+                    refreshData();
+                }
             }
         };
         registerReceiver(myReceiver, intentfilter);
@@ -73,12 +89,81 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                if (result == PermissionRequester.ALREADY_GRANTED) {
+                    Log.d("RESULT", "권한이 이미 존재함.");
+                    if (ActivityCompat.checkSelfPermission(ScrollingActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-1111-1111"));
+                        startActivity(intent);
+                    }
+                } else if (result == PermissionRequester.NOT_SUPPORT_VERSION) {
+                    Log.d("RESULT", "마쉬멜로우 이상 버젼 아님.");
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-1111-1111"));
+                    startActivity(intent);
+                } else if (result == PermissionRequester.REQUEST_PERMISSION) {
+                    Log.d("RESULT", "요청함. 응답을 기다림.");
+                    permissionRequester();
+                    Log.d("permisson",result+"");
+
+
+                }
             }
+
         });
     }
 
+    private void permissionRequester(){
+
+        result = new PermissionRequester.Builder(ScrollingActivity.this)
+                .setTitle("권한 요청")
+                .setMessage("권한을 요청합니다.")
+                .setPositiveButtonName("네")
+                .setNegativeButtonName("아니요.")
+                .create()
+                .request(Manifest.permission.CALL_PHONE, 1000, new PermissionRequester.OnClickDenyButtonListener() {
+                    @Override
+                    public void onClick(Activity activity) {
+                        Log.d("xxx", "취소함.");
+                    }
+                });
+        Log.d("permisson",result+"");
+
+    }
+
+
+
+    /**
+     * 권한 요청에 대한 응답을 이곳에서 가져온다.
+     * * @param requestCode 요청코드
+     * * @param permissions 사용자가 요청한 권한들
+     * * @param grantResults 권한에 대한 응답들(인덱스별로 매칭)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1000) { // 요청한 권한을 사용자가 "허용" 했다면...
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(ScrollingActivity.this, "권한요청을 허용했습니다.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(ScrollingActivity.this, "권한요청을 거부했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        //ToolBar Menu Icon 생성
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent intent = new Intent(ScrollingActivity.this, SettingActivity.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
 
     public void refreshData() {
         myDataset.clear();
@@ -88,7 +173,7 @@ public class ScrollingActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if(array == null) {
+        if (array == null) {
             final Handler mHandler = new Handler();
             Thread t = new Thread(new Runnable() {
                 @Override
@@ -114,7 +199,7 @@ public class ScrollingActivity extends AppCompatActivity {
                 JSONObject object;
                 try {
                     object = array.getJSONObject(i);
-                    myDataset.add(0, new NotificationData( object.getString("name"), object.getString("createdAt")));
+                    myDataset.add(0, new NotificationData(object.getString("name"), object.getString("createdAt")));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -122,15 +207,19 @@ public class ScrollingActivity extends AppCompatActivity {
             mAdapter.notifyDataSetChanged();
         }
     }
+
     public JSONArray parseJSONString(String string) throws JSONException {
         return new JSONArray(string);
     }
+
     public String requestJSONString() throws ExecutionException, InterruptedException {
         ConnManager conn = new ConnManager();
-        conn.execute("GET",ConnManager.main_url+ConnManager.log_url);
+        conn.execute("GET", ConnManager.main_url + ConnManager.log_url);
         return conn.get();
     }
-    public RecyclerView.Adapter getAdapter(){
-        return  mAdapter;
+
+    public RecyclerView.Adapter getAdapter() {
+        return mAdapter;
     }
 }
+
---
+++ Sourcecode/Android/Logger_dy/app/src/main/java/kr/ac/bist/iot_noti/Activity/SettingActivity.java
@@ -0,0 +1,15 @@
+package kr.ac.bist.iot_noti.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.ac.bist.iot_noti.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
