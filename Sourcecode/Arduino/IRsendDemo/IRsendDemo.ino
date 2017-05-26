b47c6079c79edc41f2dfcf28c503aae942a16593
Deokyun Kim <ejrdbs23@naver.com>
2017-05-27 01:35:17 +0900

    Remote Controller code edit

--- Sourcecode/Arduino/IRsendDemo/IRsendDemo.ino
+++ Sourcecode/Arduino/IRsendDemo/IRsendDemo.ino
@@ -13,7 +13,6 @@ IRsend irsend;
 int blueTx=4;   //Tx (보내는핀 설정)at
 int blueRx=5;   //Rx (받는핀 설정)
 SoftwareSerial mySerial(blueTx, blueRx);
-byte buffer[1024];
 int bufferPosition; 
 void setup()
 {
@@ -23,24 +22,24 @@ void setup()
 }
 
 void loop() {
-	for (int i = 0; i < 3; i++) {
-		irsend.sendNEC(0x616A817E, 32);
-		delay(40);
-	}
   if (mySerial.available()) {       
     //Serial.write(mySerial.read());  //블루투스측 내용을 시리얼모니터에 출력
-    byte data = mySerial.read();
-    Serial.write(data);
-    buffer[bufferPosition++] = data;
-    if(data=='\n'){
-        //buffer[bufferPosition]='\0';
-        if(!strcmp(buffer,"")){
-          
+    byte data = 0;
+    data = mySerial.read();
+    if(data > 0){
+      for (int i = 0; i < 1; i++) {
+        if(data=='1') {
+            irsend.sendNEC(0x616A817E, 32);//onoff
+        } else if(data=='2') {
+            irsend.sendNEC(0x616AE916, 32);//down
+        } else if(data=='3') {
+            irsend.sendNEC(0x616A6996, 32);//up
+        } else if(data=='4') {
+            irsend.sendNEC(0x616A9966, 32);//power
         }
-        bufferPosition=0;
+        delay(40);
+      }
+      data = 0;
     }
   }
-  if (Serial.available()) {         
-    mySerial.write(Serial.read());  //시리얼 모니터 내용을 블루추스 측에 WRITE
-  } //5 second delay between each signal burst
 }
